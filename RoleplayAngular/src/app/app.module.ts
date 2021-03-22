2281f71f7ecb0f33a29a7eb7c2d37ba726149c8c
Maarten Mateusen <mateusenmaarten@hotmail.com>
2021-03-22 21:21:55 +0100

    created home and pnf components and routing test

--- RoleplayAngular/src/app/app.module.ts
+++ RoleplayAngular/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    PageNotFoundComponent,
+    HomeComponent
+   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes,{enableTracing: true}),
   ],
   providers: [],
   bootstrap: [AppComponent]
