5ebeb1826b4d3c532fff7ed9762135d574a7e1e2
Maarten Mateusen <mateusenmaarten@hotmail.com>
2021-04-01 16:44:34 +0200

    navbar collapse on navigate fix

--- RoleplayAngular/src/app/Core/components/navbar/navbar.component.ts
+++ RoleplayAngular/src/app/Core/components/navbar/navbar.component.ts
@@ -8,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  public isCollapsed : boolean = true;
+  isCollapsed = true;
+
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
